94b01456c330ee989b8846786e6cee4ae02d2c65
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2022-10-21 15:24:43 +0200

    1. Setup UI 2. Work with logic

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var redView: UIView!
+  
+  @IBOutlet weak var yellowView: UIView!
+  
+  @IBOutlet weak var greenView: UIView!
+  
+  var traffincCounter: Int = 0
+  
+  @IBOutlet weak var startNextButtonOutlet: UIButton!
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    redView.layer.cornerRadius = 35
+    yellowView.layer.cornerRadius = 35
+    greenView.layer.cornerRadius = 35
+    
+    redView.alpha = 0.1
+    yellowView.alpha = 0.1
+    greenView.alpha = 0.1
+    
+    
+    startNextButtonOutlet.layer.cornerRadius = 20
   }
 
 
+  @IBAction func startNextPressedButton(_ sender: UIButton) {
+    sender.setTitle("Next", for: .normal)
+    
+    traffincCounter += 1
+    
+    chengeAlpha(firstView: redView, secondView: yellowView, thierdView: greenView)
+  }
+  
+  func chengeAlpha(firstView: UIView, secondView: UIView, thierdView: UIView) {
+    
+    switch traffincCounter {
+    case 1:
+      firstView.alpha = 1
+      secondView.alpha = 0.1
+      thierdView.alpha = 0.1
+    case 2:
+      firstView.alpha = 0.1
+      secondView.alpha = 1
+      thierdView.alpha = 0.1
+    case 3:
+      firstView.alpha = 0.1
+      secondView.alpha = 0.1
+      thierdView.alpha = 1
+      traffincCounter = 0
+    default:
+      print("Something is wrong")
+    }
+    
+  }
 }
 
