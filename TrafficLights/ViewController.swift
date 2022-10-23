4904cdd9cd704e810379fa04ea950f6178395b04
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2022-10-23 17:26:08 +0200

    1. Fixed UI constraints 2. Updated code

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,60 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  
   @IBOutlet weak var redView: UIView!
+  
+  
   
   @IBOutlet weak var yellowView: UIView!
   
   @IBOutlet weak var greenView: UIView!
   
-  var traffincCounter: Int = 0
-  
   @IBOutlet weak var startNextButtonOutlet: UIButton!
+  
+  var trefficCounter: Int = 0
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    redView.layer.cornerRadius = 35
-    yellowView.layer.cornerRadius = 35
-    greenView.layer.cornerRadius = 35
+    redView.layer.cornerRadius = redView.frame.height / 2
+    yellowView.layer.cornerRadius = yellowView.frame.height / 2
+    greenView.layer.cornerRadius = greenView.frame.height / 2
     
     redView.alpha = 0.1
     yellowView.alpha = 0.1
     greenView.alpha = 0.1
     
+    startNextButtonOutlet.layer.cornerRadius = 10
     
-    startNextButtonOutlet.layer.cornerRadius = 20
-  }
-
-
-  @IBAction func startNextPressedButton(_ sender: UIButton) {
-    sender.setTitle("Next", for: .normal)
-    
-    traffincCounter += 1
-    
-    chengeAlpha(firstView: redView, secondView: yellowView, thierdView: greenView)
   }
   
-  func chengeAlpha(firstView: UIView, secondView: UIView, thierdView: UIView) {
-    
-    switch traffincCounter {
-    case 1:
-      firstView.alpha = 1
-      secondView.alpha = 0.1
-      thierdView.alpha = 0.1
-    case 2:
-      firstView.alpha = 0.1
-      secondView.alpha = 1
-      thierdView.alpha = 0.1
-    case 3:
-      firstView.alpha = 0.1
-      secondView.alpha = 0.1
-      thierdView.alpha = 1
-      traffincCounter = 0
-    default:
-      print("Something is wrong")
-    }
-    
+  @IBAction func startNextButtonPressed(_ sender: UIButton) {
+    sender.setTitle("NEXT", for: .normal)
+    chengingTrafficLights(firstLight: redView, secondLight: yellowView, thierdLight: greenView)
   }
+  
+  func chengingTrafficLights(firstLight: UIView, secondLight: UIView, thierdLight: UIView) {
+    trefficCounter += 1
+    
+    switch trefficCounter {
+    case 1:
+      firstLight.alpha = 1
+      secondLight.alpha = 0.1
+      thierdLight.alpha = 0.1
+    case 2:
+      firstLight.alpha = 0.1
+      secondLight.alpha = 1
+      thierdLight.alpha = 0.1
+    case 3:
+      firstLight.alpha = 0.1
+      secondLight.alpha = 0.1
+      thierdLight.alpha = 1
+      trefficCounter = 0
+    default:
+      print("Someting is wrong")
+    }
+  }
+  
 }
-
